eef498c4206df551c0bbe6e9082d5760eff3656f
Sac Le <sinhsac@live.com>
2022-07-24 22:36:56 +0700

    update code

---
+++ source/frmMain.Designer.cs
@@ -0,0 +1,184 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace find_my_pdf
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvMain = new System.Windows.Forms.DataGridView();
+            this.clmFilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmContent = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.tsmenuGeneral = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmenuScanPDF = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtKeyword = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).BeginInit();
+            this.menuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Location = new System.Drawing.Point(1070, 40);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(144, 40);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Tìm kiếm";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvMain
+            // 
+            this.dgvMain.AllowUserToAddRows = false;
+            this.dgvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMain.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmFilePath,
+            this.clmContent,
+            this.clmStatus});
+            this.dgvMain.Location = new System.Drawing.Point(11, 96);
+            this.dgvMain.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvMain.MultiSelect = false;
+            this.dgvMain.Name = "dgvMain";
+            this.dgvMain.RowHeadersWidth = 62;
+            this.dgvMain.RowTemplate.Height = 33;
+            this.dgvMain.Size = new System.Drawing.Size(1203, 408);
+            this.dgvMain.TabIndex = 6;
+            this.dgvMain.VirtualMode = true;
+            // 
+            // clmFilePath
+            // 
+            this.clmFilePath.DataPropertyName = "pdf_path";
+            this.clmFilePath.HeaderText = "Đường dẫn";
+            this.clmFilePath.MinimumWidth = 8;
+            this.clmFilePath.Name = "clmFilePath";
+            this.clmFilePath.Width = 500;
+            // 
+            // clmContent
+            // 
+            this.clmContent.DataPropertyName = "pdf_text_content";
+            this.clmContent.HeaderText = "Nội dung";
+            this.clmContent.MinimumWidth = 8;
+            this.clmContent.Name = "clmContent";
+            this.clmContent.Width = 450;
+            // 
+            // clmStatus
+            // 
+            this.clmStatus.DataPropertyName = "status";
+            this.clmStatus.HeaderText = "Trạng thái";
+            this.clmStatus.MinimumWidth = 8;
+            this.clmStatus.Name = "clmStatus";
+            this.clmStatus.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.clmStatus.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.clmStatus.Width = 150;
+            // 
+            // menuMain
+            // 
+            this.menuMain.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuMain.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmenuGeneral});
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuMain.Size = new System.Drawing.Size(1224, 33);
+            this.menuMain.TabIndex = 7;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // tsmenuGeneral
+            // 
+            this.tsmenuGeneral.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmenuScanPDF});
+            this.tsmenuGeneral.Name = "tsmenuGeneral";
+            this.tsmenuGeneral.Size = new System.Drawing.Size(54, 29);
+            this.tsmenuGeneral.Text = "File";
+            // 
+            // tsmenuScanPDF
+            // 
+            this.tsmenuScanPDF.Name = "tsmenuScanPDF";
+            this.tsmenuScanPDF.Size = new System.Drawing.Size(190, 34);
+            this.tsmenuScanPDF.Text = "Quét PDF";
+            this.tsmenuScanPDF.Click += new System.EventHandler(this.tsmenuScanPDF_Click);
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyword.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.txtKeyword.FormattingEnabled = true;
+            this.txtKeyword.Location = new System.Drawing.Point(11, 40);
+            this.txtKeyword.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(1053, 40);
+            this.txtKeyword.TabIndex = 8;
+            this.txtKeyword.SelectedIndexChanged += new System.EventHandler(this.txtKeyword_SelectedIndexChanged);
+            this.txtKeyword.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtKeyword_KeyDown);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 513);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.menuMain);
+            this.Controls.Add(this.dgvMain);
+            this.Controls.Add(this.btnSearch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuMain;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tìm kiếm file PDF";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMain)).EndInit();
+            this.menuMain.ResumeLayout(false);
+            this.menuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Button btnSearch;
+        private DataGridView dgvMain;
+        private MenuStrip menuMain;
+        private ToolStripMenuItem tsmenuGeneral;
+        private ToolStripMenuItem tsmenuScanPDF;
+        private DataGridViewTextBoxColumn clmFilePath;
+        private DataGridViewTextBoxColumn clmContent;
+        private DataGridViewTextBoxColumn clmStatus;
+        private ComboBox txtKeyword;
+    }
+}
---
+++ source/frmMain.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace find_my_pdf
+{
+    public partial class frmMain : Form
+    {
+        SQLiteConnection _con = new SQLiteConnection();
+        string dbFileName = "xdb.xdb";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            createTable(Properties.Settings.Default.CREATE_PDF_TABLE);
+            createTable(Properties.Settings.Default.CREATE_HISTORY_TABLE);
+            countTotalPDFs();
+            loadKeywordHistories();
+        }
+
+        public void createTable(string sql)
+        {
+            if (!File.Exists(dbFileName))
+            {
+                SQLiteConnection.CreateFile(dbFileName);
+            }
+            createConection();
+            SQLiteCommand command = new SQLiteCommand(sql, _con);
+            command.ExecuteNonQuery();
+            closeConnection();
+        }
+
+        public void createConection()
+        {
+            string _strConnect = "Data Source=" + dbFileName + ";Version=3;";
+            _con.ConnectionString = _strConnect;
+            if (_con.State == ConnectionState.Closed)
+            {
+                _con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if (_con.State == ConnectionState.Open)
+            {
+                _con.Close();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchFile();
+            if (string.IsNullOrEmpty(txtKeyword.Text) || txtKeyword.Items.Contains(txtKeyword.Text))
+            {
+                return;
+            }
+            saveKeywordToHistories();
+        }
+
+        private void saveKeywordToHistories()
+        {
+            string sql = string.Format("INSERT INTO search_history(search_text) VALUES ('{0}')", txtKeyword.Text);
+            try
+            {
+                executeSQL(sql);
+            }
+            catch { }
+            finally
+            {
+                txtKeyword.Items.Add(txtKeyword.Text);
+            }
+        }
+
+        private void loadKeywordHistories()
+        {
+            createConection();
+            SQLiteCommand cmd = new SQLiteCommand(Properties.Settings.Default.LAST_TEN_HISTORIES, _con);
+            SQLiteDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                txtKeyword.Items.Add(reader.GetString(0));
+            }
+            closeConnection();
+        }
+
+        private void searchFile()
+        {
+            if (string.IsNullOrEmpty(txtKeyword.Text))
+            {
+                MessageBox.Show("Nhập dữ liệu vô mới tìm được chứ ku.", "Êh Êh Êh");
+                return;
+            }
+            string sql = string.Format(Properties.Settings.Default.PDF_SEARCH, txtKeyword.Text);
+            DataSet ds = new DataSet();
+            createConection();
+            SQLiteDataAdapter da = new SQLiteDataAdapter(sql, _con);
+            da.Fill(ds);
+            closeConnection();
+            dgvMain.DataSource = ds.Tables[0];
+        }
+
+        private void countTotalPDFs()
+        {
+            createConection();
+            SQLiteCommand cmd = new SQLiteCommand(Properties.Settings.Default.PDF_COUNTER, _con);
+            int total = Convert.ToInt32(cmd.ExecuteScalar());
+            closeConnection();
+            this.Text = String.Format(Properties.Settings.Default.LBL_BTN_QUET, total);
+        }
+
+        private void executeSQL(string sql)
+        {
+            createConection();
+            Exception ee = null;
+            try
+            {
+                SQLiteCommand cmd = new SQLiteCommand(sql, _con);
+                cmd.ExecuteNonQuery();
+            } catch (Exception ex)
+            {
+                ee = ex;
+            }
+            closeConnection();
+            if (ee != null)
+            {
+                throw ee;
+            }
+        }
+
+        private void txtKeyword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                searchFile();
+                saveKeywordToHistories();
+            }
+        }
+
+        private void tsmenuScanPDF_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            if (folderBrowser.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            string folderPath = folderBrowser.SelectedPath;
+            if (folderPath == null)
+            {
+                return;
+            }
+            string[] allfiles = Directory.GetFiles(folderPath, "*.*", SearchOption.AllDirectories);
+            if (allfiles == null || allfiles.Length == 0)
+            {
+                return;
+            }
+            int counter = 0;
+            int errorCounter = 0;
+            foreach (string fileName in allfiles)
+            {
+                if (!fileName.ToLower().EndsWith(".pdf"))
+                {
+                    continue;
+                }
+                PdfInfo pdfInfo = new PdfInfo(fileName);
+                string sql = Properties.Settings.Default.PDF_INSERT_ + pdfInfo.getValue() + ";";
+                try
+                {
+                    executeSQL(sql);
+                    counter++;
+                }
+                catch
+                {
+                    errorCounter++;
+                }
+
+            }
+
+            MessageBox.Show(string.Format("Đã quét xong {0} files PDF, trong đó \n\n{1} files thành công, \n{2} files không thành công. ",
+                allfiles.Length,
+                counter, errorCounter),
+                "Thông báo !!!");
+            countTotalPDFs();
+        }
+
+        private void txtKeyword_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            searchFile();
+        }
+    }
+}
